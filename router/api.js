558df79b7ee4dbbc7159dc67bb7a0f902a7ca018
Asthriona <im@asthriona.com>
2020-12-08 23:03:48 +0100

    fix user link

--- router/api.js
+++ router/api.js
@@ -4,7 +4,8 @@ const ShortUrl = require('../models/ShortURL');
 
 router.post('/', async(req, res) => {
     await ShortUrl.create({ full: req.body.fullUrl })
-    res.redirect('/back/user')
+    console.log(`Link created to ${req.body.fullUrl}`)
+    res.redirect('/')
 });
 
 module.exports = router
